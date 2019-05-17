584639f61cf9e43866b56aa20f0f47c007876d5b
Ronak Patel <ronak1.patel@ryerson.ca>
2019-05-16 21:45:53 -0400

    Replaced text showing user sign in with buttons and added functionality to the buttons to perform sign in and sign out.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -15,17 +15,45 @@ class GoogleAuth extends React.Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+
+          // listens for changes on isSignedIn
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
+  // called whenever user's authentication status changes
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  // sign user in
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  // sign user out
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if we are signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>;
+      return (
+        <button onClick={this.onSignOut} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 
