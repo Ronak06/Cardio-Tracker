9e15c1d4dd49f725de89a380b8f4ccd57a8e9dfc
Ronak Patel <ronak1.patel@ryerson.ca>
2019-05-18 22:10:04 -0400

    Created a form for CardioCreate component using redux-form.

--- client/src/components/caridoLogs/CardioCreate.js
+++ client/src/components/caridoLogs/CardioCreate.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const CardioCreate = () => {
-  return <div>CardioCreate</div>;
-};
+class CardioCreate extends React.Component {
+  renderInput({ input, label, type }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} type={type} />
+      </div>
+    );
+  }
 
-export default CardioCreate;
+  renderInputDistance({ input, label, type, step }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} type={type} step={step} />
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field
+          name="name"
+          component={this.renderInput}
+          label="Enter Name"
+          type="text"
+        />
+        <Field
+          name="date"
+          component={this.renderInput}
+          label="Enter Date"
+          type="date"
+        />
+        <Field
+          name="distance"
+          component={this.renderInputDistance}
+          label="Enter Distance (in miles)"
+          type="number"
+          step="0.01"
+        />
+        <Field
+          name="time"
+          component={this.renderInput}
+          label="Enter Time (e.g. 15:13)"
+          type="text"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({ form: "cardioCreate" })(CardioCreate);
