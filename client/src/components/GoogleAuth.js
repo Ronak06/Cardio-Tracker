c96063b737667a8852dfb948488a8a31ae4caa0c
Ronak Patel <ronak1.patel@ryerson.ca>
2019-05-18 20:01:59 -0400

    added userId to redux to use for later. Created variables to action types to help with debugging.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -25,7 +25,7 @@ class GoogleAuth extends React.Component {
   // called whenever user's authentication status changes
   onAuthChange = isSignedIn => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
