fb5c31798f95bc660ad2a11f749846a3f98ea23c
Ronak Patel <ronak1.patel@ryerson.ca>
2019-06-01 18:27:43 -0400

    added lodash, added delete and fetch cardiologs cases to the cardioReducer, and added the cardioReducer to the redux tree.

--- client/src/reducers/cardioReducer.js
+++ client/src/reducers/cardioReducer.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import {
   CREATE_CARDIOLOG,
   FETCH_CARDIOLOG,
@@ -8,12 +9,23 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_CARDIOLOGS:
+      // create new object with everything in state, then create an object out of it
+      // using mapKeys where the key is the id of the individual logs
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_CARDIOLOG:
+      // create new object with everything in state and the specific log using the id
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_CARDIOLOG:
+      // create new object with everything in state and the specific log using the id
       return { state, [action.payload.id]: action.payload };
     case EDIT_CARDIOLOG:
+      // create new object with everything in state and the specific log using the id
       return { state, [action.payload.id]: action.payload };
+    case DELETE_CARDIOLOG:
+      // _.omit creates a new object with everything in state
+      // without the value passed in from action.payload
+      return _.omit(state, action.payload);
     default:
       return state;
   }
