45f8a7f10cafd0a48c2c26c7d9c462b4425a79a8
Ronak Patel <ronak1.patel@ryerson.ca>
2019-06-01 18:51:10 -0400

    updated the cardioList file to fetch the list of cardio logs and display them.

--- client/src/components/caridoLogs/CardioList.js
+++ client/src/components/caridoLogs/CardioList.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchCardioLogs } from "../../actions";
 
-const CardioList = () => {
-  return <div>CardioList</div>;
+class CardioList extends React.Component {
+  componentDidMount() {
+    this.props.fetchCardioLogs();
+  }
+
+  renderList() {
+    return this.props.cardioLogs.map(cardioLog => {
+      return (
+        <div className="item" key={cardioLog.id}>
+          <i className="large middle aligned icon trophy" />
+          <div className="content">
+            {cardioLog.name}
+            <div className="description">
+              Distance: {cardioLog.distance}
+              <br />
+              Time: {cardioLog.time}
+              <br />
+              Date: {cardioLog.date}
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Cardio Logs</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { cardioLogs: Object.values(state.cardiologs) };
 };
 
-export default CardioList;
+export default connect(
+  mapStateToProps,
+  { fetchCardioLogs }
+)(CardioList);
